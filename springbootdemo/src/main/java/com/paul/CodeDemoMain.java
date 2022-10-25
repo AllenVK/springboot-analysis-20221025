ac0ae9676aa70797c5cd0997a1a350b3ba006049
Paul <q634375039@qq.com>
2022-10-25 17:04:51 +0800

    创建源码分析测试案例

---
+++ springbootdemo/src/main/java/com/paul/CodeDemoMain.java
@@ -0,0 +1,47 @@
+package com.paul;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * Date: 2022-10-25 16:49
+ * Author: Paul
+ */
+@RestController
+@SpringBootApplication
+public class CodeDemoMain {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    static Logger logger= LoggerFactory.getLogger(CodeDemoMain.class);
+
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(CodeDemoMain.class, args);
+        Environment env = applicationContext.getEnvironment();
+        logger.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://localhost:{}\n\t" +
+                        "External: \thttp://{}:{}\n\t"+
+                        "Doc: \thttp://{}:{}/doc.html\n"+
+                        "----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"));
+    }
+
+}
